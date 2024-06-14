0992bccbd40924ec62ac60f644ce8b6bdbe69415
tanaya0222 <111778605+tanaya0222@users.noreply.github.com>
2024-06-14 18:06:20 +0530

    Add files via upload

---
+++ practice.py
@@ -0,0 +1,11 @@
+#print("python \n \b world")
+#def func2(a,b):
+ #   print("avg")
+c=int(input("enter no."))  
+b=input("enter no.") 
+try:
+    print(c+b)
+except Exception as p:
+    print(p)
+print("good")
+
