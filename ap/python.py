2cecf8dcb470f9d3dd874728201c74de57ddbcab
tanaya0222 <111778605+tanaya0222@users.noreply.github.com>
2024-06-14 17:58:58 +0530

    Delete ap directory

--- ap/python.py
+++
@@ -1,7 +0,0 @@
-# Load your dataset
-df = pd.read_csv("train data.csv") 
-# Identify unique product combinations
-unique_combinations = df.groupby(['ProductType', 'Manufacturer', 'Area Code', 'Sourcing Channel', 'Product Size', 'Product Type']).size().reset_index().rename(columns={0:'NumUnits'})
-
-# Display the unique combinations and the number of units for each combination
-print(unique_combinations)
